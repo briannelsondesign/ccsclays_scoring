21e50562913ef6eb0e5a78a0dd0e64157acf5913
Kyle Daigle <kyle.daigle@github.com>
2014-03-29 15:40:59 -0400

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClayScore::Application.config.secret_key_base = '97794c3fb3cce06a2cc80431cbfa463c946670065c7793657212244969041af138298d8b662c3bc57cd76b227613c561887aa847748fae839ef6dc909087fb7a'
