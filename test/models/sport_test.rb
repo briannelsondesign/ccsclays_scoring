fb0756bfba6702fbc757e580fb3873e83030dd83
Kyle Daigle <kyle.daigle@github.com>
2014-03-29 16:04:22 -0400

    Rework sport test for clarity

--- test/models/sport_test.rb
+++ test/models/sport_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class SportTest < ActiveSupport::TestCase
 
   test "requires a name" do
-    sport = Sport.new
-    refute sport.valid?
-    sport.name = "Clay Shooting"
+    sport = Sport.make
     assert sport.valid?
+    sport.name = nil
+    refute sport.valid?
   end
 
 end
