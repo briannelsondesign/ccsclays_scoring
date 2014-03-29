561fe231ff4d304cb1f5afb6871a1abafb7de12f
Kyle Daigle <kyle.daigle@github.com>
2014-03-29 16:11:30 -0400

    Little test cleanup

--- test/models/league_test.rb
+++ test/models/league_test.rb
@@ -2,15 +2,13 @@ require 'test_helper'
 
 class LeagueTest < ActiveSupport::TestCase
 
-  describe "belongs_to :sport" do
-
+  describe "belongs_to(:sport)" do
     it "requires a Sport to be set" do
       league = League.make
       assert league.valid?
       league.sport = nil
       refute league.valid?
     end
-
   end
 
 end
