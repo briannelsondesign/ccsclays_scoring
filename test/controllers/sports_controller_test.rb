859ddecd7213144ea48c0b9282624ee9cc1b782d
Kyle Daigle <kyle.daigle@github.com>
2014-03-30 14:19:47 -0400

    Finish controller specs for Sport

--- test/controllers/sports_controller_test.rb
+++ test/controllers/sports_controller_test.rb
@@ -18,4 +18,68 @@ class SportsControllerTest < ActionController::TestCase
     end
   end
 
+  describe "POST#create" do
+    it "is successful with a complete Sport" do
+      assert_difference("Sport.count") do
+        post :create, sport: { name: "Clay Shooting" }
+      end
+      assert_redirected_to assigns(:sport)
+    end
+
+    it "is unsuccessful with an incomplete Sport" do
+      assert_no_difference("Sport.count") do
+        post :create, sport: { name: "" }
+      end
+      assert_response :success
+      assert_template :new
+    end
+  end
+
+  describe "GET#show" do
+    it "renders the Sport" do
+      sport = Sport.make!
+      get :show, id: sport.to_param
+
+      assert_response :success
+      assert_template :show
+    end
+  end
+
+  describe "GET#edit" do
+    it "renders the Sport for editing" do
+      sport = Sport.make!
+      get :edit, id: sport.to_param
+
+      assert_response :success
+      assert_template :edit
+    end
+  end
+
+  describe "PUT#update" do
+    it "is successful with a valid update to Sport" do
+      sport = Sport.make!
+      put :update, {id: sport.to_param, sport: { name: "Clay Shotting" } }
+
+      assert_redirected_to sport
+    end
+
+    it "is unsuccessful with an invalid update to Sport" do
+      sport = Sport.make!
+      put :update, {id: sport.to_param, sport: { name: "" } }
+
+      assert_response :success
+      assert_template :edit
+    end
+  end
+
+  describe "DELETE#destroy" do
+    it "deletes the sport" do
+      sport = Sport.make!
+      assert_difference("Sport.count", -1) do
+        delete :destroy, id: sport.to_param
+      end
+      assert_redirected_to sports_path
+    end
+  end
+
 end
